e24645961cb6b4225e3f2bab9cc7cffb8dc32392
kumkumhirve <kumkumhirve452@gmail.com>
2025-05-11 22:51:55 +0530

    Add files via upload

---
+++ possitive_negative_zoro.py
@@ -0,0 +1,15 @@
+'''Q1: Write a program that accepts a number from user and calculate
+whether it is positive or negative or zero'''
+
+num = int(input("enter the number:"))
+
+
+if num >0:
+
+    print("muber is positive")
+
+elif num <0:
+    print("number is negative")
+
+else:
+    print("number is zero")
